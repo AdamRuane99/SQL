fb7f2c2c900c6ec87e9c46339bf24f4f1a12e36f
Adam Ruane <87831666+AdamRuane99@users.noreply.github.com>
2021-08-28 23:29:18 +0100

    Add files via upload

---
+++ SQL Financisal Data Sandbox artificial Data .sql
@@ -0,0 +1,122 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+WITH CTE AS(
+SELECT 
+	 EOMONTH(convert(datetime, [DATE], 103)) AS [Month End], MONTH(convert(datetime, [DATE], 103)) AS Month_Number, DATENAME(Month, convert(datetime, [DATE], 103)) AS [Month_Name], Datepart(Year, convert(datetime, [DATE], 103)) AS Year
+	,CASE [Country] WHEN '1' THEN 'England' WHEN '4' THEN 'France' WHEN '2' THEN 'Ireland' END AS Country 
+      ,[CONTRACT]
+      ,[PRODUCT]
+    
+   , 
+SUM(TRY_CONVERT(FLOAT,[Invested Units])) AS [Invested Units],
+SUM(TRY_CONVERT(FLOAT,[Units Value])) AS [Units Value]
+,
+     [CODE]
+      ,[DESCRIPTION Code]
+      
+  
+  
+  FROM dbo.[Financial_data_sandbox_query_artificial_data_inflows]
+  WHERE 
+ [DESCRIPTION Code] IN ('SPI', 'INSTALLMENT', 'FIRST PREMIUM', 'COUPON RE-INVESTMENT')
+ GROUP BY [CONTRACT]
+      ,[PRODUCT]
+      
+   , 
+    [Code]
+      ,[DESCRIPTION Code]
+      , [Date], Country, [Invested units]
+  ),
+
+CTE2 AS(
+SELECT DISTINCT  POLICY_SHORT_NUMBER, POLICY_ID, POLICY_NUMBER
+  FROM dbo.policy_bridge) , 
+
+  CTE3 AS ( 
+  SELECT [Month End], Month_Name, Month_Number, [YEAR], Country,  Product, , [Code], [DESCRIPTION Code], [Invested Units], [Units Value], POLICY_ID, POLICY_NUMBER
+  FROM CTE
+  LEFT JOIN CTE2 ON   CTE.[CONTRACT] = CTE2.POLICY_SHORT_NUMBER)
+
+  ,
+  Inflow  AS(
+  SELECT  [Month End], Month_Name, Month_Number, [YEAR], [Code], [DESCRIPTION Code], [Invested Units] [Units], [Units Value] 
+ , ( [Invested Units] * [Units Value])  [Amount],
+  Country,  Product [Product Code], POLICY_NUMBER
+  FROM CTE3)
+  ,
+  Tab1 AS(
+SELECT 
+	  EOMONTH(convert(datetime, [DATE], 103)) AS [Month End], MONTH(convert(datetime, [DATE], 103)) AS Month_Number, DATENAME(Month, convert(datetime, [DATE], 103)) AS [Month_Name], Datepart(Year, convert(datetime, [DATE], 103)) AS Year
+	,CASE [Country] WHEN '1' THEN 'England' WHEN '4' THEN 'France' WHEN '2' THEN 'Ireland' END AS Country 
+      ,[CONTRACT]
+      ,[PRODUCT]
+     
+   , 
+SUM(TRY_CONVERT(FLOAT,[DISINVESTED UNITS])) AS [Units],
+SUM(TRY_CONVERT(FLOAT,[LAST VALUE ])) AS [Units Value] ,
+SUM(TRY_CONVERT(FLOAT,[AMOUNT     ])) AS Amount
+,
+     [CODE]
+      ,[DESCRIPTION CODE]
+    
+  
+  
+  FROM dbo.[Financial_data_sandbox_query_artificial_data_outflows]
+  WHERE 
+ [DESCRIPTION CODE] IN ('Surrender', 'PARTIAL SURRENDER'   , 'MATURITY')
+
+ GROUP BY [CONTRACT]
+      ,[PRODUCT]
+  
+,
+     [CODE]
+      ,[DESCRIPTION CODE]
+     , [Date], Country
+  ),
+
+  Tab2 AS(
+SELECT DISTINCT  POLICY_SHORT_NUMBER, POLICY_ID, POLICY_NUMBER
+  FROM dbo.[Client_to_policy_bridge]) , 
+
+  Tab3 AS ( 
+  SELECT [Month End], Month_Name, Month_Number, [YEAR], Country,  Product, [Code], [DESCRIPTION CODE], [Units], [Units Value], POLICY_ID, POLICY_NUMBER
+  FROM Tab1
+  LEFT JOIN  Tab2 ON   Tab1.[CONTRACT] =  Tab2.POLICY_SHORT_NUMBER), 
+
+
+    Outflow AS(
+
+  SELECT  [Month End], Month_Name, Month_Number, [YEAR], [Code], [DESCRIPTION CODE], [Units], [Units Value] 
+ , ( [Units] * [Units Value])  [Amount],
+  Country,  Product [Product Code], POLICY_NUMBER
+  FROM Tab3)
+ ,
+InflowOutflow AS(
+ SELECT * 
+ FROM Inflow
+ UNION 
+ SELECT * 
+ FROM Outflow)
+
+
+ , 
+ InflowOutflowTable AS(
+ SELECT 
+ [Month End], Month_Name, Month_Number, [YEAR],  [Code], [DESCRIPTION CODE], [Units], [Units Value] 
+ ,  [Amount],
+  Country,   [Product Code], POLICY_NUMBER,
+ 
+ SUM(CASE WHEN [DESCRIPTION CODE] IN ('Surrender', 'PARTIAL SURRENDER'   , 'MATURITY') THEN Amount END) AS [Out Flows], 
+ SUM(CASE WHEN [DESCRIPTION  CODE] IN 'SPI', 'INSTALLMENT', 'FIRST PREMIUM', 'COUPON RE-INVESTMENT') THEN Amount END) AS [In Flows]
+ FROM InflowOutflow
+
+  GROUP BY 
+ 
+ [Month End], Month_Name, Month_Number, [YEAR], [Code], [DESCRIPTION CODE], [Units], [Units Value] 
+ ,  [Amount],
+  Country,   [Product Code], POLICY_NUMBER )
+
+  ,
+  Final_CTE AS(
+  SELECT *,  (ISNULL([In Flows], 0) - ISNULL([Out Flows], 0)) AS [Net Flows]
+  FROM InflowOutflowTable) 
+
